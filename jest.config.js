eb3ed8bdaee522e6e28a8fc23d9bec53c763d3b3
Alex Thalhammer <alex@angular-software.at>
2021-09-12 20:46:07 +0200

    Initial commit: labs repo with updated packages

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/flight-app',
+    '<rootDir>/libs/flight-lib',
+    '<rootDir>/libs/logger-lib',
+    '<rootDir>/libs/dashboard-lib',
+    '<rootDir>/apps/flight-admin',
+    '<rootDir>/apps/dashboard',
+    '<rootDir>/apps/external',
+    '<rootDir>/libs/passenger/domain',
+    '<rootDir>/apps/passenger',
+    '<rootDir>/libs/passenger/feature-search',
+    '<rootDir>/libs/passenger/feature-edit',
+  ],
+};
