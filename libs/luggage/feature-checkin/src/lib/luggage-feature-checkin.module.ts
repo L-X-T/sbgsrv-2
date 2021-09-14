5bdcb0c944c91c8c8b03ef53f17b07e602627bd0
Alex Thalhammer <alex@angular-software.at>
2021-09-14 09:15:26 +0200

    [NxDDD] Bonus: Configure a Shared Utility Library

--- libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
+++ libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
 import { CheckinComponent } from './checkin.component';
 import { LuggageUiCardModule } from '@flight-workspace/luggage/ui-card';
+import { LoggerModule } from '@flight-workspace/logger-lib';
 
 @NgModule({
-  imports: [CommonModule, LuggageDomainModule, LuggageUiCardModule, RouterModule.forChild([{ path: '', component: CheckinComponent }])],
+  imports: [CommonModule, LuggageDomainModule, LuggageUiCardModule, LoggerModule, RouterModule.forChild([{ path: '', component: CheckinComponent }])],
   declarations: [CheckinComponent],
   exports: [CheckinComponent]
 })
