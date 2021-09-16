9d085bd95ce157fa2c2513bebb4835d86607e012
Alex Thalhammer <alex@angular-software.at>
2021-09-16 11:10:39 +0200

    [RxJs] Combine Streams

--- apps/flight-app/src/app/flight-lookahead/flight-lookahead.component.ts
+++ apps/flight-app/src/app/flight-lookahead/flight-lookahead.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Flight } from '@flight-workspace/flight-lib';
-import { Observable } from 'rxjs';
+import { combineLatest, interval, Observable } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { debounceTime, distinctUntilChanged, filter, map, pairwise, switchMap, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, map, pairwise, startWith, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'flight-workspace-flight-lookahead',
@@ -16,17 +16,37 @@ export class FlightLookaheadComponent implements OnInit {
   diff$: Observable<number>;
   loading: boolean;
 
+  online = false;
+  online$: Observable<boolean>;
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.control = new FormControl();
+    const input$ = this.control.valueChanges.pipe(debounceTime(300));
 
-    this.flights$ = this.control.valueChanges.pipe(
+    /*this.flights$ = this.control.valueChanges.pipe(
       debounceTime(300),
       filter((input) => input.length > 2),
       distinctUntilChanged(),
       tap((input) => (this.loading = true)),
       switchMap((input) => this.load(input)),
+      tap((v) => (this.loading = false))
+    );*/
+
+    this.online$ = interval(2000).pipe(
+      startWith(0),
+      map((_) => Math.random() < 0.5),
+      distinctUntilChanged(),
+      tap((value) => (this.online = value))
+    );
+
+    this.flights$ = combineLatest([input$, this.online$]).pipe(
+      filter(([, online]) => online),
+      map(([input]) => input),
+      distinctUntilChanged(),
+      tap((input) => (this.loading = true)),
+      switchMap((input: string) => this.load(input)),
       tap((v) => (this.loading = false))
     );
 
